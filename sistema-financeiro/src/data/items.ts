7eb7ed52be88c45214efa1afea5398a0e69e5629
Rafael S. de Lima <rafael.lima@startse.com>
2022-12-29 06:25:30 -0300

    rebot project

--- sistema-financeiro/src/data/items.ts
+++
@@ -1,8 +0,0 @@
-import {Item} from '../types/Item'
-
-export const items: Item[] = [
-    {date: new Date(2022, 9,15), category: 'food', title: 'McDonald', value: 12.50},
-    {date: new Date(2022, 9,15), category: 'food', title: 'McDonald', value: 85.50},
-    {date: new Date(2022, 9,16), category: 'rent', title: 'Aluguel', value: 1200.50},
-    {date: new Date(2022, 10,7), category: 'salary', title: 'Salário', value: 14712.50}
-];
